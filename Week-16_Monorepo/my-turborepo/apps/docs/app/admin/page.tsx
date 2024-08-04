01f935895e668c57ec6701a0be909527ffd9dadc
YashXBansal <yashbansal414@gmail.com>
2024-08-04 19:18:02 +0530

    intro to turborepos

---
+++ Week-16_Monorepo/my-turborepo/apps/docs/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import { Admin } from "@repo/ui/Admin";
+
+export default function () {
+  return (
+    <div>
+      Hi this is from docs <Admin />
+    </div>
+  );
+}
---
+++ Week-16_Monorepo/my-turborepo/apps/web/app/admin/page.tsx
@@ -0,0 +1,8 @@
+import { Admin } from "@repo/ui/Admin"
+export default function() {
+    return <div>
+        Hi from 
+        <Admin />
+    </div>
+
+}
